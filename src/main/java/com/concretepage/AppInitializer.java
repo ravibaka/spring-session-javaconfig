510d19f661c00222f6d85b058d82b3b95c793ffd
ravibaka <ravi.baka@accenture.com>
2019-03-05 09:53:19 +0100

    adding redis spring session example to git repo

---
+++ src/main/java/com/concretepage/AppInitializer.java
@@ -0,0 +1,7 @@
+package com.concretepage;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+public class AppInitializer extends AbstractHttpSessionApplicationInitializer {
+	public AppInitializer() {
+		super(AppConfig.class);
+	}
+}
